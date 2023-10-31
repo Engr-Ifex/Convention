db48166957373449d5d11c15d441b97b07e008a2
Engr_ifex <ayobamiadesoji2@gmail.com>
2023-10-30 18:27:23 -0700

    complete

--- second.js
+++ second.js
@@ -2,11 +2,25 @@ let showimg = document.querySelector('#showimg')
 let pho = localStorage.getItem('updateimg')
 let para = document.querySelector('p')
 let nm = localStorage.getItem('name')
+let main = document.querySelector('#mainimg')
 
 para.innerHTML = nm;
 showimg.src = pho;
 
-console.log(pho)
+
+
+function htmlToimage() {
+
+
+    domtoimage.toJpeg(document.getElementById('mainimg'), { quality: 0.95 })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'myImage.jpeg';
+        link.href = dataUrl;
+        link.click();
+    });
+}
+
 
 
 
